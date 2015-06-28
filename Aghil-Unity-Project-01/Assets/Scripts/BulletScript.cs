68105cb3d265cdf313406cfd4ed8abb89a1ab293
miti <biyarjomandi@gmail.com>
2015-06-28 14:34:27 +0430

    shoot function

---
+++ Aghil-Unity-Project-01/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public float BulletSpeed;
+
+	void Start(){
+		Destroy (gameObject, 6);
+	}
+	void Update () {
+		transform.Translate (Vector3.right * BulletSpeed * Time.deltaTime);
+	}
+}
---
+++ Aghil-Unity-Project-01/Assets/Scripts/ShootManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootManager : MonoBehaviour {
+
+	public Animator MG42Animator;
+	public GameObject MG42Bullet;
+	public float MG42Stray;
+	public Transform ShootPointMG42;
+	private float NextShot;
+	private bool StartShoot;
+	public float FireRate;
+	// Use this for initialization
+	void Start () {
+	
+		MG42Animator.StopPlayback ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (StartShoot)
+			InvokeRepeating ("FireWeapon", FireRate, FireRate);
+
+	
+	}
+
+	void FireWeapon()
+		
+	{
+		
+		if(Time.time > NextShot)
+			
+		{
+
+			MG42Animator.SetBool("Shoot",true);
+			var randomNumberX = Random.Range(-MG42Stray, MG42Stray);
+			var randomNumberY = Random.Range(-MG42Stray, MG42Stray);
+			var randomNumberZ = Random.Range(-MG42Stray, MG42Stray);
+			GameObject YourProjectileClone =(GameObject)Instantiate(MG42Bullet,ShootPointMG42.position,ShootPointMG42.rotation); // instantiate projectile
+			YourProjectileClone.transform.Rotate(randomNumberX, randomNumberY, randomNumberZ);
+			NextShot = Time.time + 0.1f;
+		}
+		StartShoot = true;
+		CancelInvoke ("FireWeapon");
+}
+	void StopFire(){
+		MG42Animator.SetBool("Shoot",false);
+		StartShoot = false;
+		CancelInvoke ("FireWeapon");
+	}
+}
