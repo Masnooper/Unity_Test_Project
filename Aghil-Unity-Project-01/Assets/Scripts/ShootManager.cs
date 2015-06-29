8296946fde43e6fa809c15fb1a269865b40fd4e8
miti <biyarjomandi@gmail.com>
2015-06-29 11:44:17 +0430

    MG42 function edited
    also muzzle flash added . bullet removed and hi to target added too.

--- Aghil-Unity-Project-01/Assets/Scripts/ShootManager.cs
+++ Aghil-Unity-Project-01/Assets/Scripts/ShootManager.cs
@@ -3,47 +3,65 @@ using System.Collections;
 
 public class ShootManager : MonoBehaviour {
 
+	public Transform MG42Body;
 	public Animator MG42Animator;
-	public GameObject MG42Bullet;
-	public float MG42Stray;
+	private float MG42Stray;
+	public float MG42MaxStray;
 	public Transform ShootPointMG42;
-	private float NextShot;
 	private bool StartShoot;
 	public float FireRate;
+	public float RecoilPower;
+	public GameObject MuzzleFlash;
+	private Quaternion ShootpointorginRot;
 	// Use this for initialization
 	void Start () {
 	
 		MG42Animator.StopPlayback ();
+		ShootpointorginRot = ShootPointMG42.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	if (StartShoot)
 			InvokeRepeating ("FireWeapon", FireRate, FireRate);
+		else 
+			ShootPointMG42.rotation = ShootpointorginRot;
 
-	
+
 	}
 
 	void FireWeapon()
 		
 	{
-		
-		if(Time.time > NextShot)
-			
-		{
+	
 
 			MG42Animator.SetBool("Shoot",true);
+			MuzzleFlash.SetActive (true);
 			var randomNumberX = Random.Range(-MG42Stray, MG42Stray);
 			var randomNumberY = Random.Range(-MG42Stray, MG42Stray);
 			var randomNumberZ = Random.Range(-MG42Stray, MG42Stray);
-			GameObject YourProjectileClone =(GameObject)Instantiate(MG42Bullet,ShootPointMG42.position,ShootPointMG42.rotation); // instantiate projectile
-			YourProjectileClone.transform.Rotate(randomNumberX, randomNumberY, randomNumberZ);
-			NextShot = Time.time + 0.1f;
-		}
+			RaycastHit hit;
+			ShootPointMG42.Rotate(randomNumberX, randomNumberY, randomNumberZ);
+			Vector3 FWD = ShootPointMG42.TransformDirection(Vector3.right);
+			if (Physics.Raycast (ShootPointMG42.position, FWD, out hit)){
+				Debug.DrawLine(ShootPointMG42.position, hit.point,Color.black,3);
+				if(hit.collider.tag=="Target"){
+					hit.transform.SendMessage ("TakeDamage",true,SendMessageOptions.DontRequireReceiver);}
+			}
+			//GameObject YourProjectileClone =(GameObject)Instantiate(MG42Bullet,ShootPointMG42.position,ShootPointMG42.rotation); // instantiate projectile
+			//YourProjectileClone.transform.Rotate(randomNumberX, randomNumberY, randomNumberZ);
+
+		if (MG42Stray < MG42MaxStray)
+			MG42Stray += 0.01f;
+
+		MG42Body.Rotate(-MG42Stray*RecoilPower*Time.deltaTime,Random.Range(MG42Stray*RecoilPower,-MG42Stray*RecoilPower)*Time.deltaTime,0,Space.Self);
+
 		StartShoot = true;
 		CancelInvoke ("FireWeapon");
 }
 	void StopFire(){
+		MG42Stray = 0f;
+		MuzzleFlash.SetActive (false);
 		MG42Animator.SetBool("Shoot",false);
 		StartShoot = false;
 		CancelInvoke ("FireWeapon");
---
+++ Aghil-Unity-Project-01/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetScript : MonoBehaviour {
+
+	public float health = 100;
+	public float MG42DamageDone;
+
+	void TakeDamage(){
+		health -= MG42DamageDone;
+		if (health < 0) {
+			print ("target destroyed");
+			Destroy (gameObject, 0.2f);
+		}
+	}
+}
